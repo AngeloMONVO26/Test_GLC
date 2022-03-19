c84fd3fe0a56755e3ccda08cee71a9d7aa7bf3ad
Angelo Monvo <angelo.monvo@epitech.eu>
2022-03-19 23:27:31 +0100

    [ADD] Code pour trouver le gagnant du jeu

---
+++ test_gls.js
@@ -0,0 +1,47 @@
+let j1 = {
+    "nom" : "Kodjo",
+    "notes" : [12, 8, 15, 10, 11]
+}
+
+let j2 = {
+    "nom" : "Awa",
+    "notes" : [14, 14, 9, 6, 13]
+}
+
+function winner(player1, player2){
+    let point1 = 0
+    let point2 = 0
+
+    if (player1.notes.length == player2.notes.length){
+        for (let i = 0; i < player1.notes.length; i++) {            
+            if (typeof(player1.notes[i]) == 'number' && typeof(player2.notes[i]) == 'number'){
+                if (player1.notes[i] < player2.notes[i]) {
+                    point2 = point2 + 1
+                }
+                else if (player1.notes[i] > player2.notes[i]) {
+                    point1 = point1 + 1
+                }
+            } else {
+                console.log("Erreur : veuillez entrer des notes valides.")
+                return 0
+            }
+        }
+    
+    
+        console.log(player1.nom + ": " + point1 + " points")
+        console.log(player2.nom + ": " + point2 + " points")
+        
+        if (point1 < point2){
+            console.log("Le gagnant du concours est " + player2.nom)
+        }
+        else if (point1 > point2){
+            console.log("Le gagnant du concours est" + player1.nom)
+        } else{
+            console.log("Match nul")
+        }
+    } else {
+        console.log("Les joueurs n'ont pas le même nombre de points.")
+    }
+}
+
+winner(j1, j2)
